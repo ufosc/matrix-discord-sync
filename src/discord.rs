9a324d8977ac89585bfc83102daf1b768142c2d5
Hunter Jarrell <hjarrell555@gmail.com>
2020-06-16 00:44:24 -0400

    Support sending events from discord side on channel changes

--- src/discord.rs
+++ src/discord.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Cow, env, sync::Arc};
+use std::{env, sync::Arc, sync::mpsc};
 
 use serenity::framework::standard::{
     macros::{command, group},
@@ -13,22 +13,31 @@ use serenity::{
     prelude::*,
 };
 
-use log::info;
+use log::{error, info};
 
-enum ChannelEvent {
+pub enum ChannelEvent {
     NewChannel(GuildChannel),
     UpdatedChannel(GuildChannel, GuildChannel),
     DeletedChannel(GuildChannel),
 }
 
+struct SharedDataContainer;
+
+impl TypeMapKey for SharedDataContainer {
+    type Value = Arc<Mutex<mpsc::Sender<ChannelEvent>>>;
+}
+
 struct Handler;
 impl EventHandler for Handler {
-    fn channel_create(&self, _ctx: Context, channel: Arc<RwLock<GuildChannel>>) {
+    fn channel_create(&self, ctx: Context, channel: Arc<RwLock<GuildChannel>>) {
         let c = unwrap_and_copy_channel(&channel);
-        handle_new_channel(c);
+        let tx = get_tx_clone(&ctx);
+        std::thread::spawn(move || {
+            handle_new_channel(c, &tx);
+        });
     }
 
-    fn channel_update(&self, _ctx: Context, old: Option<Channel>, new: Channel) {
+    fn channel_update(&self, ctx: Context, old: Option<Channel>, new: Channel) {
         if old.is_none() {
             return;
         }
@@ -41,15 +50,21 @@ impl EventHandler for Handler {
         if let Channel::Guild(wrap_new_channel) = new {
             let old_channel = unwrap_and_copy_channel(&wrap_old_channel);
             let new_channel = unwrap_and_copy_channel(&wrap_new_channel);
-            handle_updated_channel(&old_channel, &new_channel);
+            let tx = get_tx_clone(&ctx);
+            std::thread::spawn(move || {
+                handle_updated_channel(old_channel, new_channel, &tx); 
+            });
         } else {
             return;
         }
     }
 
-    fn channel_delete(&self, _ctx: Context, channel: Arc<RwLock<GuildChannel>>) {
+    fn channel_delete(&self, ctx: Context, channel: Arc<RwLock<GuildChannel>>) {
         let c = unwrap_and_copy_channel(&channel);
-        handle_deleted_channel(c);
+        let tx = get_tx_clone(&ctx);
+        std::thread::spawn(move || {
+            handle_deleted_channel(c, &tx);
+        });
     }
 }
 
@@ -57,33 +72,58 @@ pub fn unwrap_and_copy_channel(wrapped_channel: &Arc<RwLock<GuildChannel>>) -> G
     (*wrapped_channel.read()).clone()
 }
 
-pub fn handle_new_channel(channel: GuildChannel) {
+pub fn get_tx_clone(ctx: &Context) -> mpsc::Sender<ChannelEvent> {
+    let data = ctx.data.read();
+    let tx_mutex = data.get::<SharedDataContainer>().expect("Error retrieving SharedDataContainer");
+    let tx = tx_mutex.lock();
+    tx.clone()
+}
+
+pub fn handle_new_channel(channel: GuildChannel, tx: &mpsc::Sender<ChannelEvent>) {
     info!(
         "Channel {} created with ID {} and server {}\n",
         channel.name, channel.id, channel.guild_id
     );
+    match tx.send(ChannelEvent::NewChannel(channel)) {
+        Ok(_) => (),
+        Err(_) => error!("Error sending NewChannel event to matrix"),
+    };
 }
 
-pub fn handle_updated_channel(new_channel: &GuildChannel, old_channel: &GuildChannel) {
+pub fn handle_updated_channel(old_channel: GuildChannel, new_channel: GuildChannel, tx: &mpsc::Sender<ChannelEvent>) {
     info!(
         "Channel {} updated with ID {} and server {}. Now called {}\n",
-        new_channel.name,
-        new_channel.id,
-        new_channel.guild_id,
-        old_channel.name()
+        old_channel.name,
+        old_channel.id,
+        old_channel.guild_id,
+        new_channel.name()
     );
+    match tx.send(ChannelEvent::UpdatedChannel(old_channel, new_channel)) {
+        Ok(_) => (),
+        Err(_) => error!("Error sending UpdatedChannel event to matrix"),
+    };
 }
 
-pub fn handle_deleted_channel(channel: GuildChannel) {
+pub fn handle_deleted_channel(channel: GuildChannel, tx: &mpsc::Sender<ChannelEvent>) {
     info!(
         "Channel {} deleted with ID {} and server {}\n",
         channel.name, channel.id, channel.guild_id
     );
+    match tx.send(ChannelEvent::DeletedChannel(channel)) {
+        Ok(_) => (),
+        Err(_) => error!("Error sending DeletedChannel event to matrix"),
+    };
 }
 
-pub fn init() {
+pub fn init(tx: mpsc::Sender<ChannelEvent>) {
     let token = env::var("DISCORD_TOKEN").expect("Expected a DISCORD_TOKEN in the environment");
     let mut client = Client::new(&token, Handler).expect("Error creating Discord client");
+
+    {
+        let mut data = client.data.write();
+        data.insert::<SharedDataContainer>(Arc::from(Mutex::new(tx.clone())));
+    }
+
     client.with_framework(
         StandardFramework::new().configure(|c| c.prefix("~")),
     );
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc;
+
 use log::{error, info};
 
 mod discord;
@@ -6,10 +8,33 @@ fn main() {
     kankyo::init().expect("Failed to load .env file.\nSee .env.example in project root.");
     env_logger::init();
 
+    let (tx_to_matrix, rx_matrix) = mpsc::channel();
+
     info!("Starting Discord bot");
     let discord_thread = std::thread::spawn(move || {
-        discord::init();
+        discord::init(tx_to_matrix);
+    });
+
+    let matrix_thread = std::thread::spawn(move || {
+        loop {
+            match rx_matrix.recv() {
+                Ok(event) => print_event(event),
+                Err(_e) => {
+                    error!("Error recieiving message from Discord.");
+                    panic!("");
+                }
+            };
+        }
     });
 
     discord_thread.join().expect("Error joining Discord bot thread.");
+    matrix_thread.join().expect("error joining matrix");
+}
+
+fn print_event(event: discord::ChannelEvent) {
+    match event {
+        discord::ChannelEvent::NewChannel(nc) => info!("New channel event!"),
+        discord::ChannelEvent::UpdatedChannel(nc1, nc2) => info!("Updated channel event!"),
+        discord::ChannelEvent::DeletedChannel(nc) => info!("Deleted channel event!"),
+    }
 }
