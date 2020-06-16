cf6f43e7b1689e0a8b4ca0855bd2f1dd748bf3cf
Hunter Jarrell <hjarrell555@gmail.com>
2020-06-16 01:23:40 -0400

    Add manual command for syncing room

--- src/discord.rs
+++ src/discord.rs
@@ -5,6 +5,7 @@ use serenity::{
     framework::{
         StandardFramework,
         standard::{
+            Args,
             macros::{
                 command,
                 group,
@@ -15,6 +16,7 @@ use serenity::{
     model::{
         channel::Channel,
         channel::GuildChannel,
+        channel::Message,
     },
     prelude::*,
 };
@@ -135,6 +137,36 @@ pub fn handle_deleted_channel(http: Arc<Http>, channel: GuildChannel, tx: &mpsc:
     };
 }
 
+#[command]
+// Limits the usage of this command to roles named:
+#[allowed_roles("officer")]
+fn sync(ctx: &mut Context, msg: &Message, _args: Args) -> CommandResult {
+    if let Some(channel) = msg.channel(&ctx.cache) {
+        match channel {
+            Channel::Guild(gc) => {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "Requesting matrix bridged room...") {
+                    error!("Error sending message: {:?}", why);
+                } else {
+                    let c = unwrap_and_copy_channel(&gc);
+                    info!("Manual channel creation requested by: {} for {}", msg.author.name, c.name);
+                    let tx = get_tx_clone(&ctx);
+                    let http = ctx.http.clone();
+                    std::thread::spawn(move || {
+                        let http = http.clone();
+                        handle_new_channel(http, c, &tx);
+                    });
+                }
+            },
+            _ => (),
+        };
+    }
+    Ok(())
+}
+
+#[group]
+#[commands(sync)]
+struct Admin;
+
 pub fn init(tx: mpsc::Sender<DiscordToMatrixMsg>) {
     let token = env::var("DISCORD_TOKEN").expect("Expected a DISCORD_TOKEN in the environment");
     let mut client = Client::new(&token, Handler).expect("Error creating Discord client");
@@ -159,7 +191,8 @@ pub fn init(tx: mpsc::Sender<DiscordToMatrixMsg>) {
             .configure(|c| c
                 .owners(owners)
                 .prefix("~")
-            ),
+            )
+            .group(&ADMIN_GROUP),
     );
     if let Err(why) = client.start() {
         println!("Err with client: {:?}", why);
